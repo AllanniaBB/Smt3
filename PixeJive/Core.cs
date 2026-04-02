8c102667ca45c481f25c931da58d24e0c8b1c56b
AllanniaBB <236902269+AllanniaBB@users.noreply.github.com>
2026-04-02 09:38:25 +0100

    Add files via upload

---
+++ PixeJive/Core.cs
@@ -0,0 +1,22 @@
+﻿using Il2Cpp;
+using Il2Cppresult2_H;
+using MelonLoader;
+using static Il2Cpp.SteamDlcFileUtil;
+
+[assembly: MelonInfo(typeof(PixeJive.Core), "PixeJive", "1.0.0", "Allannia", null)]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace PixeJive
+{
+    public class Core : MelonPlugin
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+            tblSkill.fclSkillTbl[61].Event[7] = new fclSkillParam_t { Param = 421, TargetLevel = 0, Type = 1 }; // Counter
+        }
+    }
+}
+
+
+
